4dd08c706bf03c08c56bb8084134be16d3746f43
Kendy <132912361+Kendysse@users.noreply.github.com>
2023-05-10 10:52:20 -0300

    Update index.php

--- index.php
+++ index.php
@@ -2,11 +2,12 @@
 <html>
   <head>
     <title>Canto da Casa</title>
+    <link rel="stylesheet" href="personalizaçao.css"
   </head>
   <body>
     <h1>Canto da Casa</h1>
     <p>Salve salve, bem vindo(a) ao site! Aqui você descontará sua frustração e jogara no Canto da Casakkkk.</p>
-    <p>Sobre mim: Sou um(a) aluno(a) inciante de clouf computing!
+    <p>Sobre mim: Sou um(a) aluno(a) inciante de cloud computing!
       Gosto de jogar uns jogos, assistir uns filmes, tocar guitarra e ouvir música.</p>
     <a href="#" class="button">Entre em contato</a>
   </body>
