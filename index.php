421cf53adfcc6d6b92e1240e8d3d7f6ba6269d68
Kendy <132912361+Kendysse@users.noreply.github.com>
2023-05-10 12:02:23 -0300

    Update index.php

--- index.php
+++ index.php
@@ -9,6 +9,11 @@
     <p>Salve salve, bem vindo(a) ao site! Aqui você descontará sua frustração e jogara no Canto da Casakkkk.</p>
     <p>Sobre mim: Sou um(a) aluno(a) inciante de cloud computing!
       Gosto de jogar uns jogos, assistir uns filmes, tocar guitarra e ouvir música.</p>
+    <p>Nietzsche defendia a inexistência em vários sentidos: de Deus, da alma e do sentido da vida. Para ele, o ser humano deveria abandonar as muletas metafísicas, a chamada morte dos ídolos. O filósofo se opunha aos dogmas da sociedade, principalmente ao defender que a verdade era uma ilusão.</p>
+    <?php
+    date_default_timezone_set('America/Sao_Paulo');
+    echo date('h:i:s A');
+    ?>
     <a href="https://www.youtube.com/watch?v=dQw4w9WgXcQ" class="button">Entre em contato</a>
   </body>
 </html>
