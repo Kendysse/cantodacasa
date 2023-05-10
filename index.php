85b2eb7de33972820bf39c4454e68ebada02750c
Kendy <132912361+Kendysse@users.noreply.github.com>
2023-05-10 10:44:46 -0300

    Update index.php

--- index.php
+++ index.php
@@ -1,3 +1,13 @@
-<?php
-echo "Salve az 900";
-?>
+<!DOCTYPE html>
+<html>
+  <head>
+    <title>Canto da Casa</title>
+  </head>
+  <body>
+    <h1>Canto da Casa</h1>
+    <p>Salve salve, bem vindo(a) ao site! Aqui você descontará sua frustração e jogara no Canto da Casakkkk.</p>
+    <p>Sobre mim: Sou um(a) aluno(a) inciante de clouf computing!
+      Gosto de jogar uns jogos, assistir uns filmes, tocar guitarra e ouvir música.</p>
+    <a href="#" class="button">Entre em contato</a>
+  </body>
+</html>
